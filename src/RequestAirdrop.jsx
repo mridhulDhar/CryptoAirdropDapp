9726294ee9fe53ea88bd9ba103bc03991a487bb3
mridhulDhar <mdhar006@gmail.com>
2025-07-06 13:59:05 -0400

    basic airdrop dapp commit

---
+++ src/RequestAirdrop.jsx
@@ -0,0 +1,18 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export function RequestAirdrop(){
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    function requestAirdrop(){
+        const publicKey = wallet.publicKey;
+        const amount = document.getElementById("amount").value;
+        connection.requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL);
+    }
+
+    return <div>
+        <input id="amount" type="text" placeholder="Amount.."></input><br />
+        <button onClick={requestAirdrop}>Request Airdrop</button>
+    </div>
+}
